e30e0b1eabfad4e18025412fce2d2cc25042b1f8
lzudor <lzudor@omnitech-engineering.net>
2023-06-14 18:58:31 +0300

    ShadowRoot Test

--- src/app/ferris-wheel/ferris-wheel.component.ts
+++ src/app/ferris-wheel/ferris-wheel.component.ts
@@ -1,10 +1,11 @@
-import { OnInit, Component } from '@angular/core';
+import { OnInit, Component, ViewEncapsulation } from '@angular/core';
 import initAnimation from './ferris-wheel-animation.js';
 
 @Component({
   selector: 'app-ferris-wheel',
   templateUrl: './ferris-wheel.component.html',
-  styleUrls: ['./ferris-wheel.component.css']
+  styleUrls: ['./ferris-wheel.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class FerrisWheelComponent {
     ngOnInit(): void {
--- src/app/svgator-logo/svgator-logo.component.ts
+++ src/app/svgator-logo/svgator-logo.component.ts
@@ -1,11 +1,12 @@
-import { OnInit, Component } from '@angular/core';
+import {OnInit, Component, ViewEncapsulation} from '@angular/core';
 import initAnimation from './svgator-logo-animation.js';
 
 
 @Component({
   selector: 'app-svgator-logo',
   templateUrl: './svgator-logo.component.html',
-  styleUrls: ['./svgator-logo.component.css']
+  styleUrls: ['./svgator-logo.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class SvgatorLogoComponent {
     ngOnInit(): void {
